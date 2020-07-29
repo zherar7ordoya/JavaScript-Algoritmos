a19a9cc06c2d45cd6c0f24775078b16a753e117c
GERARDO TORDOYA <zherar7ordoya@outlook.com>
2020-07-29 20:20:54 -0300

    Update inventory-update.js

--- inventory-update.js
+++ inventory-update.js
@@ -1,60 +1,82 @@
-const updateInventory = (arr1, arr2) => {
-    const validateArray = (arr, param) => {
-        if (arr.some(x => !Array.isArray(x))) {
-            throw new Error(`${param} is not a 2D array.`);
-        }
+/**
+ * Actualiza una colección
+ * Se respeta el nombre original de la función a los fines de la evaluación
+ * Repasa el array de datos nuevos y los manda a buscar en el array en uso
+ * @param {*[]} arr1 Inventario en uso
+ * @param {*[]} arr2 Datos nuevos
+ */
 
-        if (arr.some(x => x.length !== 2)) {
-            throw new Error(`${param} is formated incorrectly. All items in 2D array must contain exactly 2 items.`);
-        }
-
-        const msg = 'is formated incorrectly. Expecting every %num% item in each 2D array to be a';
-
-        if (arr.some(x => typeof x[0] !== 'number')) {
-            throw new Error(`${param} ${msg.slice().replace('%num%', '1st')} number.`);
-        }
-
-        if (arr.some(x => typeof x[1] !== 'string')) {
-            throw new Error(`${param} ${msg.slice().replace('%num%', '2nd')} string.`);
-        }
-    }
-
-    validateArray(arr1, 'Parameter 1');
-    validateArray(arr2, 'Parameter 2');
-
-    const getEntry = (name) => {
-        for (const item of arr1) { if (item[1] === name) return item; }
-    }
-
-    arr2.forEach(x => {
-        const match = getEntry(x[1]);
-        if (match) {
-            match[0] = match[0] + x[0];
-        } else {
-            arr1.push(x);
-        }
-    });
-
-    arr1.sort((a, b) => {
-        const x = a[1].toLowerCase();
-        const y = b[1].toLowerCase();
-        if (x < y) { return -1; }
-        if (x > y) { return 1; }
-        return 0;
-    });
-
-    return arr1;
+function updateInventory(arr1, arr2) {
+    arr2.forEach(item => {
+        inventariar(arr1, item)
+    })
+    return arr1
 }
 
 /**
- * @returns [
- *              [1, "Bowling Ball"],
- *              [0, "Dirty Sock"],
- *              [1, "Hair Pin"],
- *              [1, "Half-Eaten Apple"],
- *              [0, "Microphone"],
- *              [1, "Toothpaste"]
- *          ]
+ * Busca el artículo en el array en uso. Si lo encuentra, actualiza la cantidad
+ * que le corresponde. Si no lo encuentra, agrega artículo y cantidad.
+ * @param {*} arr1 El inventario en uso completo
+ * @param {*} item El par (sub-array) de datos nuevos
  */
+const inventariar = (arr1, item) => {
+    let index = -1
+    // Tener en mente que compara un registro por vez del array en uso
+    while (++index < arr1.length) {
+        // Si el artículo es igual al artículo, sumar nueva cantidad y regresar
+        if (arr1[index][1] === item[1]) {
+            arr1[index][0] += item[0]
+            return
+        }
+        /**
+         * Ordenador. El mejor que he visto.
+         * while no encontró coincidencia. Si alfabéticamente el artículo del 
+         * array en uso es mayor que el item, escapa de while y lo entrega a 
+         * splice (que lo agrega en esa posición)
+         */
+        if (arr1[index][1] > item[1]) {
+            break
+        }
+    }
+    /**
+     * Sin coincidencia, agregar
+     * splice() cambia el contenido de un array eliminando elementos existentes
+     * y/o agregando nuevos elementos.
+     * 
+     * Sintaxis: array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+     * 
+     * start: Índice donde se comenzará a cambiar el array (con 0 como origen).
+     *        Si es mayor que la longitud del array, el punto inicial será la 
+     *        longitud del array. Si es negativo, empezará esa cantidad de 
+     *        elementos contando desde el final.
+     * deleteCount: Si deleteCount es igual a 0 o negativo, no se eliminará 
+     *              ningún elemento. En este caso, se debe especificar al menos 
+     *              un nuevo elemento
+     * item: los elementos que se agregarán al array
+     */
+    arr1.splice(index, 0, item)
+}
 
-console.log(updateInventory([[0, "Bowling Ball"], [0, "Dirty Sock"], [0, "Hair Pin"], [0, "Microphone"]], [[1, "Hair Pin"], [1, "Half-Eaten Apple"], [1, "Bowling Ball"], [1, "Toothpaste"]]))
+
+// Inventario en uso
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+]
+
+// Datos nuevos
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+]
+
+
+// Prueba de consola
+console.log(updateInventory(curInv, newInv))
+
+// Función definitiva
+updateInventory(curInv, newInv)
